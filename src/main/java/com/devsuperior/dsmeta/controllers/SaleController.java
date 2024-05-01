2df6b37056f0c20b19a2c251464cdffc0120a7ff
claudiobelempb <claudio.c.lima@hotmail.com>
2024-05-01 08:00:47 -0300

    Desafio Atualizado

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,17 @@
 package com.devsuperior.dsmeta.controllers;
 
+import com.devsuperior.dsmeta.dto.RelatorioVendasDTO;
+import com.devsuperior.dsmeta.dto.SumarioVendasDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -16,7 +19,7 @@ public class SaleController {
 
 	@Autowired
 	private SaleService service;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
 		SaleMinDTO dto = service.findById(id);
@@ -24,14 +27,20 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<RelatorioVendasDTO>> getReport(@RequestParam(name = "minDate", required = false) String minDate,
+															  @RequestParam(name = "maxDate", required = false) String maxDate,
+															  @RequestParam(name = "name", defaultValue = "") String name,
+															  Pageable pageable) {
+
+		Page<RelatorioVendasDTO> relDTO = service.relatorioVendaPaginado(minDate, maxDate, name, pageable);
+		return ResponseEntity.ok(relDTO);
 	}
 
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
+	public ResponseEntity<List<SumarioVendasDTO>> getSummary(@RequestParam(name = "minDate", required = false) String minDate,
+															 @RequestParam(name = "maxDate", required = false) String maxDate) {
+		List<SumarioVendasDTO> sumarioDTO = service.sumarioVenda(minDate, maxDate);
+
+		return ResponseEntity.ok(sumarioDTO);
 	}
 }
